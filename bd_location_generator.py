b0881e67cc3947ee061a369f8dde0fe07e6e793f
Konrad Hafen <khafen74@gmail.com>
2017-07-03 16:33:55 -0700

    updates to dialog

--- bd_location_generator.py
+++ bd_location_generator.py
@@ -21,7 +21,7 @@
  ***************************************************************************/
 """
 from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon
+from PyQt4.QtGui import QAction, QIcon, QToolBar, QMenu
 # Initialize Qt resources from file resources.py
 import resources
 # Import the code for the dialog
